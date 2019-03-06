c8a73f95acf3a2df9337de02c286ecac836cc2ba
p0po <w-y-p-x-l@163.com>
2019-03-06 11:31:51 +0800

    Update Uuid.java

--- Uuid.java
+++ Uuid.java
@@ -12,7 +12,9 @@ public class Uuid {
     static SimpleDateFormat dateFm = new SimpleDateFormat("yyyyMMddHHmmssSSS");
     static int[] numArray = new int[90000];
 
-    //TODO 保证乱序不重复
+    //TODO 1.保证乱序不重复
+    //TODO 2.保证随机步伐自增
+    //TODO 3.会动态更新
     static {
         Random random = new Random();
         for(int i=0;i<numArray.length;i++){
@@ -22,10 +24,12 @@ public class Uuid {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
+        //TODO 线程安全
         int index = 0;
         for(int i=0;i<10000000;i++){
             Calendar calendar = Calendar.getInstance();
             Date now = calendar.getTime();
+            //TODO "1"该生成器唯一标识，做到可配置
             String uuid = dateFm.format(now)+"1"+numArray[index++];
             if(index>=89999){
                 index = 0;
